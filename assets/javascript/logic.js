ecbeea9b81cccc8a534c989fb320a903cf49f409
Brainybrian316 <brainybrian316@gmail.com>
2022-03-27 11:16:51 -0400

    created function to fetch api and respond

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,9 @@
 // make call to weather api
-var getWeather = function () {
-    console.log('function was called')
+const getWeather = function () {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=1057c30581fce41e7df886393bc1cbde").then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    })
 }
-
 getWeather();
